e923d08076ea68fbc5b627d6d30ed73c1196aa6e
Raul Dias <raulmendonca11@gmail.com>
2022-08-29 11:41:36 +0000

    1006 finalizada

---
+++ beecrowd/1006.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        double A,B,C;
+        double media;
+        A = sc.nextDouble();
+        B = sc.nextDouble();
+        C = sc.nextDouble();
+        
+        media = (A*2 + B*3 + C*5)/10;
+        
+        System.out.printf("MEDIA = %.1f\n",media);
+
+    }
+ 
+}
